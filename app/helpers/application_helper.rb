e1fa3dcb75fa10277884d84d7c6a69842d60cec1
juan267 <j.gomez267@gmail.com>
2017-01-06 14:34:25 -0500

    finish guard

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+
+  def full_title(title = '')
+    base_title = 'Ruby on Rails Tutorial'
+    if title.empty?
+      base_title
+    else
+      "#{title} | #{base_title}"
+    end
+  end
+
+
+
 end
