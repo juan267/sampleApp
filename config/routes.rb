5a492b05eff3b51259288a772b6277f9cbc63469
juan267 <j.gomez267@gmail.com>
2017-01-10 13:06:41 -0500

    finish authentication

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,10 @@ Rails.application.routes.draw do
   get 'signup', to: 'users#new'
   post 'signup', to: 'users#create'
 
+  # Sessions
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+
   resources :users
 end
